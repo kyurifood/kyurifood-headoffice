272780aba84ba323aee83323997623ce067d2448
anggi riyandi <anggi.riyandi@gmail.com>
2016-05-16 15:50:08 +0700

    strukture folder

--- app/src/main/java/com/artivisi/android/kyurifood/headoffice/activities/MainActivity.java
+++ app/src/main/java/com/artivisi/android/kyurifood/headoffice/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.artivisi.android.kyurifood.headoffice;
+package com.artivisi.android.kyurifood.headoffice.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.artivisi.android.kyurifood.headoffice.R;
 
 public class MainActivity extends AppCompatActivity {
 
