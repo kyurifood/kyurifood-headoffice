e42fcce573809ba61f7de3ce7e959a8cf0a15505
nandakimam <nandakhairul123@gmail.com>
2016-05-18 17:19:24 +0700

    registrasi distributor head_office

--- app/src/main/java/com/artivisi/android/kyurifood/headoffice/fragments/DistributorFragment.java
+++ app/src/main/java/com/artivisi/android/kyurifood/headoffice/fragments/DistributorFragment.java
@@ -1,5 +1,7 @@
 package com.artivisi.android.kyurifood.headoffice.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,8 +9,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.artivisi.android.kyurifood.headoffice.R;
+import com.artivisi.android.kyurifood.headoffice.activities.MainActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +36,12 @@ public class DistributorFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    EditText txtkode, txtnama, txthp, txtalamat, txtkodepos, txtemail, txtpas, txtcpas;
+    Button btn_bank;
+    Dialog addBank;
+    EditText txtrek,txtpemilik,txtcabang;
+    Spinner spinnerbank;
 
     public DistributorFragment() {
         // Required empty public constructor
@@ -59,16 +72,82 @@ public class DistributorFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_distributor, container, false);
+        final View view =  inflater.inflate(R.layout.fragment_distributor, container, false);
+        txtkode = (EditText)view.findViewById(R.id.editkode);
+        if (txtkode.getText().toString().length() == 0){
+            txtkode.setError("anjrit");
+        }
+
+
+        btn_bank = (Button) view.findViewById(R.id.btnbank);
+        btn_bank.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                showAddBank();
+            }
+
+            private void showAddBank() {
+                addBank = new Dialog(getActivity());
+                addBank.setContentView(R.layout.bank_distributor);
+                addBank.setTitle("Add Bank");
+                addBank.setCancelable(false);
+                addBank.show();
+
+                final Spinner mSpinner= (Spinner)view.findViewById(R.id.spinnerbank);
+                txtrek = (EditText) view.findViewById(R.id.editText);
+                txtpemilik = (EditText) view.findViewById(R.id.editText2);
+                txtcabang = (EditText) view.findViewById(R.id.editText3);
+                spinnerbank = (Spinner) view.findViewById(R.id.spinnerbank);
+
+                Button but = (Button) addBank.findViewById(R.id.cancel);
+                but.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addBank.dismiss();
+                    }
+                });
+                Button ok = (Button) addBank.findViewById(R.id.ok);
+                ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addBank.dismiss();
+                    }
+                });
+
+
+
+
+            }
+
+
+
+
+        });
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
+
+
+        // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
